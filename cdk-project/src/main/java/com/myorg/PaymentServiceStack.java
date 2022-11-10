d3f9847b73f17b372722785dceb76f0bcac7c101
hide-tech <hidetechyy@gmail.com>
2022-11-10 16:07:54 +0500

    Add payment service stack

--- cdk-project/src/main/java/com/myorg/PaymentServiceStack.java
+++ cdk-project/src/main/java/com/myorg/PaymentServiceStack.java
@@ -1,8 +1,22 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.dynamodb.*;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcProps;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateServiceProps;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.sqs.QueueProps;
 import software.constructs.Construct;
+
+import java.util.Map;
 
 public class PaymentServiceStack extends Stack {
 
@@ -19,6 +33,79 @@ public class PaymentServiceStack extends Stack {
                                final String secret) {
         super(scope, id, props);
 
+        Queue queue = new Queue(this, "PaymentQueue", QueueProps.builder()
+                .fifo(true)
+                .contentBasedDeduplication(true)
+                .queueName("PaymentQueue")
+                .build());
 
+        paymentQueueUrl = queue.getQueueUrl();
+
+        Vpc vpc = new Vpc(this, "payment-service-vpc", VpcProps.builder()
+                .maxAzs(1)
+                .natGateways(1)
+                .build());
+
+        Cluster cluster = new Cluster(this, "payment-service-cluster", ClusterProps.builder()
+                .clusterName("paymentServiceCluster")
+                .vpc(vpc)
+                .build());
+
+        Table dynamoTable = new Table(this, "payment-service-db", TableProps.builder()
+                .tableName("invoices")
+                .partitionKey(Attribute.builder()
+                        .type(AttributeType.STRING)
+                        .name("id")
+                        .build())
+                .billingMode(BillingMode.PROVISIONED)
+                .build());
+
+        Map<String, String> containerEnv = Map.of(
+                "CLOUD_AWS_ACCESS-KEY", props.getEnv().getAccount(),
+                "CLOUD_AWS_SECRET-KEY", secret,
+                "CLOUD_AWS_REGION", props.getEnv().getRegion(),
+                "CLOUD_AWS_DYNAMO_ENDPOINT", dynamoTable.getTableName(),
+                "CLOUD_AWS_QUEUE_RECEIVE-ENDPOINT", paymentQueueUrl,
+                "CLOUD_AWS_QUEUE_SEND-ENDPOINT", orderQueueUrl);
+
+        ApplicationLoadBalancedFargateService serviceApp = new ApplicationLoadBalancedFargateService(
+                this, "payment-service-load-balancer", ApplicationLoadBalancedFargateServiceProps.builder()
+                .cluster(cluster)
+                .desiredCount(2)
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder()
+                        .image(ContainerImage.fromAsset("../back/payment-service/payment-service",
+                                AssetImageProps.builder()
+                                        .buildArgs(containerEnv)
+                                        .build()))
+                        .containerPort(8080)
+                        .build())
+                .build()
+        );
+
+        dynamoTable.grantReadWriteData(serviceApp.getTaskDefinition().getTaskRole());
+
+        serviceApp.getTargetGroup().configureHealthCheck(HealthCheck.builder()
+                .port("traffic-port")
+                .path("/actuator/health")
+                .interval(Duration.seconds(5))
+                .timeout(Duration.seconds(4))
+                .healthyThresholdCount(2)
+                .unhealthyThresholdCount(2)
+                .healthyHttpCodes("200")
+                .build());
+
+        ScalableTaskCount scalableTaskCount = serviceApp.getService().autoScaleTaskCount(EnableScalingProps.builder()
+                .maxCapacity(4)
+                .minCapacity(2)
+                .build());
+
+        scalableTaskCount.scaleOnCpuUtilization("cpu-autoscaling", CpuUtilizationScalingProps.builder()
+                .targetUtilizationPercent(45)
+                .policyName("cpu-autoscaling-policy")
+                .scaleInCooldown(Duration.seconds(30))
+                .scaleOutCooldown(Duration.seconds(30))
+                .build());
     }
 }
